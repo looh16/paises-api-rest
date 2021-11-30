9115e397ff142ba7a03adcd37af6480027d2db2d
clanga <custodio.langa@cedsif.gov.mz>
2021-11-30 11:14:34 +0200

    remocao de configuracoes da base de dados remota

--- api.rest/src/main/java/paises/api/rest/DatabaseConfig.java
+++ api.rest/src/main/java/paises/api/rest/DatabaseConfig.java
@@ -1,23 +1,23 @@
 package paises.api.rest;
 
-import com.zaxxer.hikari.HikariConfig;
-import com.zaxxer.hikari.HikariDataSource;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+//import com.zaxxer.hikari.HikariConfig;
+//import com.zaxxer.hikari.HikariDataSource;
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//import javax.sql.DataSource;
 
-import javax.sql.DataSource;
-
-@Configuration
+//@Configuration
 public class DatabaseConfig {
 
-    @Value("${spring.datasource.url}")
-    private String dbUrl;
-
-    @Bean
-    public DataSource dataSource() {
-        HikariConfig config = new HikariConfig();
-        config.setJdbcUrl(dbUrl);
-        return new HikariDataSource(config);
-    }
+//    @Value("${spring.datasource.url}")
+//    private String dbUrl;
+//
+//    @Bean
+//    public DataSource dataSource() {
+//        HikariConfig config = new HikariConfig();
+//        config.setJdbcUrl(dbUrl);
+//        return new HikariDataSource(config);
+//    }
 }
